20b240b3974d40e8adcdbe5c3af22133f393fe4e
Jessica Hormazabal <arcoirisky@MacBook-Pro-de-Jessica.local>
2023-10-24 12:17:37 -0300

    refactor(listView-from-list)

--- my_app/fl_components/lib/screens/listview1_scree.dart
+++ my_app/fl_components/lib/screens/listview1_scree.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
   const Listview1Screen({Key? key}) : super(key: key);
 
   @override
@@ -8,13 +15,12 @@ class Listview1Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: const Text('Listview tipo 1')),
         body: ListView(
-          children: const [
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo')
+          children: [
+            ...options
+                .map((game) => ListTile(
+                    title: Text(game),
+                    trailing: const Icon(Icons.arrow_forward_ios_outlined)))
+                .toList(),
           ],
         ));
   }
