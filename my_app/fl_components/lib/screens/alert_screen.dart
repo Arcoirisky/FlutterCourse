78136d29f5ea5cd7453072858fea90b404af9902
Jessica Hormazabal <arcoirisky@MacBook-Pro-de-Jessica.local>
2023-10-25 11:48:54 -0300

    feat(alert-android/IOs)

--- my_app/fl_components/lib/screens/alert_screen.dart
+++ my_app/fl_components/lib/screens/alert_screen.dart
@@ -1,14 +1,105 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 5,
+          title: const Text('Alerta'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es un mensaje de alerta'),
+              SizedBox(
+                height: 10,
+              ),
+              FlutterLogo(
+                size: 100,
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Alerta'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es un mensaje de alerta'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              CupertinoDialogAction(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'Cancelar',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              CupertinoDialogAction(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Ok'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AlertScreen'),
-      ),
-    );
+    return Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Text('Mostrar alerta', style: TextStyle(fontSize: 16)),
+            ),
+            onPressed: () => Platform.isAndroid
+                ? displayDialogAndroid(context)
+                : displayDialogIOS(context),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ));
   }
 }
--- my_app/fl_components/lib/theme/app_theme.dart
+++ my_app/fl_components/lib/theme/app_theme.dart
@@ -4,20 +4,30 @@ class AppTheme {
   static const primaryColor = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: primaryColor,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: primaryColor,
-      elevation: 0,
-    ),
-    listTileTheme: const ListTileThemeData(
-      iconColor: primaryColor,
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        primary: primaryColor,
+      primaryColor: primaryColor,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: primaryColor,
+        elevation: 0,
       ),
-    ),
-  );
+      listTileTheme: const ListTileThemeData(
+        iconColor: primaryColor,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          primary: primaryColor,
+        ),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: primaryColor,
+        elevation: 5,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          primary: primaryColor,
+          shape: const StadiumBorder(),
+          elevation: 0,
+        ),
+      ));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
     primaryColor: primaryColor,
