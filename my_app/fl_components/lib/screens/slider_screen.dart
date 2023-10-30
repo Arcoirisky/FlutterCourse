b73f3171af234d2512647c806fdc8a8ceca71281
Jessica Hormazabal <arcoirisky@MacBook-Pro-de-Jessica.local>
2023-10-30 11:38:56 -0300

    feature(switchs/checks)

--- my_app/fl_components/lib/screens/slider_screen.dart
+++ my_app/fl_components/lib/screens/slider_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class SliderScreen extends StatefulWidget {
@@ -9,37 +10,72 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
-
+  bool _sliderEnabled = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Slider & Checks Screen'),
       ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Slider.adaptive(
-            divisions: 10,
-            min: 50,
-            max: 400,
-            value: _sliderValue,
-            onChanged: (value) {
-              _sliderValue = value;
-              setState(() {});
-            },
-          ),
-          Image(
-            image: const NetworkImage(
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpKONMrV07kAaUazSdDsUHDTg5aZzok7p2Yg&usqp=CAU",
+      body: Column(children: [
+        Slider.adaptive(
+          divisions: 10,
+          min: 50,
+          max: 800,
+          activeColor: AppTheme.primaryColor,
+          inactiveColor: AppTheme.primaryColor.withOpacity(0.3),
+          thumbColor: AppTheme.primaryColor,
+          value: _sliderValue,
+          onChanged: _sliderEnabled
+              ? (value) {
+                  _sliderValue = value;
+                  setState(() {});
+                }
+              : null,
+        ),
+        Checkbox(
+          value: _sliderEnabled,
+          onChanged: (value) {
+            _sliderEnabled = value ?? true;
+            setState(() {});
+          },
+        ),
+        CheckboxListTile(
+          activeColor: AppTheme.primaryColor,
+          title: const Text('Habilitar slider'),
+          value: _sliderEnabled,
+          onChanged: (value) => setState(() {
+            _sliderEnabled = value ?? true;
+          }),
+        ),
+        Switch(
+          value: _sliderEnabled,
+          onChanged: (value) => setState(() {
+            _sliderEnabled = value;
+          }),
+        ),
+        SwitchListTile.adaptive(
+          activeColor: AppTheme.primaryColor,
+          title: const Text('Habilitar slider'),
+          value: _sliderEnabled,
+          onChanged: (value) => setState(() {
+            _sliderEnabled = value;
+          }),
+        ),
+        const AboutListTile(),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Image(
+              image: const NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpKONMrV07kAaUazSdDsUHDTg5aZzok7p2Yg&usqp=CAU",
+              ),
+              fit: BoxFit.contain,
+              width: _sliderValue,
+              height: _sliderValue,
             ),
-            fit: BoxFit.contain,
-            width: _sliderValue,
           ),
-          const SizedBox(
-            height: 100,
-          ),
-        ]),
-      ),
+        ),
+      ]),
     );
   }
 }
